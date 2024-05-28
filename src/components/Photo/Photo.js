8ce44df9f2da4010d4e47b63c6844624f1c21dea
JinwooKim <rlawlsdn9583@gmail.com>
2024-05-28 18:06:40 +0900

    fix: import name

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import './index.css';
+import './Photo.css';
 
 const Photo = () => {
   return (

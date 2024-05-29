7767c1cf4433b7fb58787792ce82bf25c740b165
jihoon135 <kwakjihoon1001@gmail.com>
2024-05-29 16:39:23 +0900

    modify: add css dir, signup router

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import LoginPage from "./components/Login";
 import ImageUploadPage from "./pages/imageupload";
 import MainPage from "./pages/main";
 import Lookbook from "./pages/lookbook";
+import SignupPage from "./components/Signup";
+
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/Signup" element={<SignupPage />} />
         <Route path="/ImageUpload" element={<ImageUploadPage />} />
         <Route path="/Main" element={<MainPage />} />
         <Route path="/lookbook" element={<Lookbook />} />

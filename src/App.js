7762f5aadf13d2cd0a25015d669b34033a917fe3
jihoon135 <kwakjihoon1001@gmail.com>
2024-05-29 16:08:58 +0900

    test 로직 취소

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/Layout";
 import LoginPage from "./components/Login";
-import SignupPage from "./pages/signup";
 
 function App() {
   return (
     <Layout>
         <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
         </Routes>
     </Layout>
   );
